5361bec7e8f2fccd7959da416d88d85983b7e133
Sergey <serega2033@gmail.com>
2018-11-20 23:58:15 +0200

    Refactoring
    Added adaptive on 320px and fixed menu navbar

--- js/main.js
+++ js/main.js
@@ -2,54 +2,55 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /*****************************************************MENU*************************************************************/
 
-    var mainMenu = document.getElementsByClassName('main-menu')[0];
+var mainMenu = document.getElementsByClassName('main-menu')[0];
 
-    window.onscroll = function() {
-        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrolled >= 2) {
-            mainMenu.classList.remove('main-menu');
-            mainMenu.classList.add('white-menu');
-            mainMenu.classList.add('black-letter');
-        }
-        if (scrolled === 0) {
-            mainMenu.classList.add('main-menu');
-            mainMenu.classList.remove('white-menu');
-            mainMenu.classList.remove('black-letter');
-        }
-    };
-
-    var buttonMenu = document.getElementById('button-menu');
-    var close = document.getElementsByClassName('close')[0];
-    var navbar = document.getElementsByClassName('nav-collapse')[0];
-
-    buttonMenu.addEventListener('click', function () {
-        navbar.style.display = ('block');
-    });
-
-    close.addEventListener('click', function () {
-        navbar.style.display = ('none');
-    });
+window.onscroll = function () {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled >= 2) {
+        mainMenu.classList.remove('main-menu');
+        mainMenu.classList.add('white-menu');
+        mainMenu.classList.add('black-letter');
+    }
+    if (scrolled === 0) {
+        mainMenu.classList.add('main-menu');
+        mainMenu.classList.remove('white-menu');
+        mainMenu.classList.remove('black-letter');
+    }
+};
 
 /********************************************************SCROLL********************************************************/
 
 var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 1200);
-        return false;
-    });
+$('a[href*="#"]').click(function () {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1200);
+    return false;
+});
 
 /********************************************************SLIDER********************************************************/
 
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,5000);
+var slideInterval = setInterval(nextSlide, 5000);
 
-function nextSlide(){
+function nextSlide() {
     slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
+    currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].className = 'slide showing';
 }
+
+/************************************************MOBILE MENU WRAPPER***************************************************/
+$('#button-menu').on('click', function () {
+    $('.nav-collapse').slideToggle(500);
+    $('body', 'html').css('overflow', 'hidden');
+});
+
+$('.close, .navbar-nav li a').on('click', function () {
+    $('.nav-collapse').slideToggle(500);
+    $('body', 'html').css('overflow', '');
+});
+
+
 
 });
